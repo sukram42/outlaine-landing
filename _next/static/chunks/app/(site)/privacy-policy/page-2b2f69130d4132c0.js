2a4d7a0012111e44e322bd84c642db5a429340b1
sukram42 <markus.boebel@tum.de>
2024-11-17 12:37:30 +0000

    Deploying to gh-pages from @ sukram42/outlaine-landing@423b8fd6a59565e6fe000c3c280ea91dd2af1ef5 🚀

---
+++ _next/static/chunks/app/(site)/privacy-policy/page-2b2f69130d4132c0.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[436],{9208:function(e,r,t){Promise.resolve().then(t.bind(t,1561))},1561:function(e,r,t){"use strict";t.r(r);var n=t(7437),o=t(2265);r.default=()=>{let[e,r]=o.useState(!1);return(o.useEffect(()=>{r(!0)},[]),e)?(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("section",{id:"support",className:"px-4 md:px-8 2xl:px-0",children:(0,n.jsxs)("div",{className:"relative mx-auto max-w-c-1390 px-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20",children:[(0,n.jsx)("h1",{children:"Privacy Policy"}),"hallo we talk about the privacy Policz and so on"]})})}):null}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),l=Object.prototype.hasOwnProperty,c=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,a={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,s={},i=null,u=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(u=r.ref),r)l.call(r,n)&&!a.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:i,ref:u,props:s,_owner:c.current}}r.Fragment=s,r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=9208)}),_N_E=e.O()}]);
