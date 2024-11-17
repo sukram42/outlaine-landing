5fd354c08418cf25ef876181724ab100cfff35ef
Markus Böbel <markus.boebel@tum.de>
2024-11-17 14:51:56 +0100

    nojekyll file

--- next.config.js
+++ next.config.js
@@ -6,17 +6,6 @@ const nextConfig = {
   output: 'export',
   swcMinify: true,
   distDir: "_next",
-  generateBuildId: async () => { if (process.env.BUILD_ID) { return process.env.BUILD_ID; } else { return `${new Date().getTime()}`; } },
-  // images: {
-  //   domains: ["localhost"],
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "cdn.sanity.io",
-  //       port: "",
-  //     },
-  //   ],
-  // },
 };
 
 module.exports = nextConfig;
