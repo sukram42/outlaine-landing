82339f85bb6813c4e6e2691b63d1e95166b36908
Markus Böbel <markus.boebel@tum.de>
2024-11-17 11:18:50 +0100

    initial commit

---
+++ app/(site)/privacy-policy/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Contact from "@/components/Contact";
+import { Metadata } from "next";
+import PrivacyPolicy from "@/components/PrivacyPolicy";
+
+export const metadata: Metadata = {
+    title: "Support Page - Solid SaaS Boilerplate",
+    description: "This is Support page for Solid Pro",
+    // other metadata
+};
+
+const PrivacyPolicyPage = () => {
+    return (<div className="pb-20 pt-4"> <PrivacyPolicy/></div>);
+};
+
+export default PrivacyPolicyPage;
---
+++ components/PrivacyPolicy/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import React from "react";
+
+const Contact = () => {
+  /**
+   * Source: https://www.joshwcomeau.com/react/the-perils-of-rehydration/
+   * Reason: To fix rehydration error
+   */
+  const [hasMounted, setHasMounted] = React.useState(false);
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      {/* <!-- ===== Contact Start ===== --> */}
+      <section id="support" className="px-4 md:px-8 2xl:px-0">
+        <div className="relative mx-auto max-w-c-1390 px-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
+         <h1>Privacy Policy</h1>
+          hallo we talk about the privacy Policz and so on
+        </div>
+      </section>
+      {/* <!-- ===== Contact End ===== --> */}
+    </>
+  );
+};
+
+export default Contact;
